3f25d6c82c30e57eb2e35bc057ba90ca59c6c363
Felipe6677 <felipeprates@colegiopaulodetarso.com.br>
2026-02-04 16:06:33 +0000

    hello

---  comentario.c
+++  comentario.c
@@ -1,8 +1,13 @@
-#include <stdio.h>  
+Para que serve #include <stdio.h>Onde o programa começa ?
 
-int main(void)      
-{
-    printf("Hello, World!\n"); 
+ela é feita pra que o programa consiga interagir com o usuario
+e ela começa a ser executado no main.
 
-    return 0;      
-o
+O que acontece se remover o return 0?
+
+não acontece nada de mais e o programa roda normalmente
+
+O que acontece se remover o \n
+
+faz com que o conteúdo subsequente seja exibido na mesma linha,
+em vez de pular para a próxima.
